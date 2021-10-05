dd0896e69a5d65d472231203cd53d0297b30b524
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2021-10-05 11:39:03 -0400

    piece generation made for the other side
    board now has pieces generated on the other side of the board at startup.

--- Checkers/Assets/Scripts/PlayerController.cs
+++ Checkers/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject piece;
+    public GameObject redPiece;
+    public GameObject blackPiece;
+    private int[] blackX = new int[12] { 0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7 };
+    private int[] blackY = new int[12] { 7, 5, 6, 7, 5, 6, 7, 5, 6, 7, 5, 6 };
     private int[] redX = new int[12] { 0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7 };
-    private int[] redY = new int[12] {1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
+    private int[] redY = new int[12] { 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0 };
     private int i = 0;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +26,8 @@ public class PlayerController : MonoBehaviour
     void setupPieces(){
         
         for (i = 0; i < 12; i++) {
-            var newPiece = Instantiate(piece, new Vector3(redX[i]*2, redY[i]*2, 0), Quaternion.identity);
+            var newPieceRed = Instantiate(redPiece, new Vector3(redX[i]*2, redY[i]*2, 0), Quaternion.identity);
+            var newPieceBlack = Instantiate(blackPiece, new Vector3(blackX[i] * 2, blackY[i] * 2, 0), Quaternion.identity);
         }
     }
 }
