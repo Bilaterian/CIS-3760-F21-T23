c3cf59e1308cb7bf12a7483eac8bfbaef5aec8d3
Evan Cooper <ecooper@clearsoftware.ca>
2021-10-05 20:52:14 -0400

    add pause menu with resume and main menu options.

--- Checkers/Assets/Scripts/ChangeScene.cs
+++ Checkers/Assets/Scripts/ChangeScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
@@ -8,6 +9,6 @@ public class ChangeScene : MonoBehaviour
 
 
     public void ChangeToScene(string sceneToChangeTo) {
-        Application.LoadLevel(sceneToChangeTo);
+        SceneManager.LoadScene(sceneToChangeTo);
     }
 }
