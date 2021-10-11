3e94b557aa22aaf9c1f847abcfdfc865572168d6
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2021-10-11 12:01:55 -0400

    Added partial movement
    Still buggy, but pieces are now moving.
    Issues:
    - multiple pieces can be selected during a turn
    - move tiles do not despawn after piece is moved
    -pieces can go out of bounds. checks need to be made when spawning move tiles
    -black tiles are layered behind the move tiles, but this can be fixed once move tiles despawn after moving them.

--- Checkers/Assets/Scripts/Piece.cs
+++ Checkers/Assets/Scripts/Piece.cs
@@ -7,31 +7,51 @@ public class Piece : MonoBehaviour //so far this only highlights the pieces
     SpriteRenderer thisSprite;
     Color colorOrig;
     [SerializeField] private int teamColor; //0 equals black, 1 equals red
-    private bool moveMe;
+    private Vector3 mousePos;
+    [SerializeField] private MoveTile moveTile;
+
+    private MoveTile tile1;
+    private MoveTile tile2;
+    private MoveTile tile3;
+    private MoveTile tile4;
 
     void Start(){
         thisSprite = GetComponent<SpriteRenderer>();
         colorOrig = thisSprite.color;
-        moveMe = false;
     }
 
     // Start is called before the first frame update
     void OnMouseOver(){
-        thisSprite.color = Color.yellow;
+         thisSprite.color = Color.yellow;
     }
 
     void OnMouseExit() {
-        thisSprite.color = colorOrig;
+         thisSprite.color = colorOrig;
     }
 
-    void OnMouseClick() {
-        moveMe = true;
-        thisSprite.color = Color.yellow;
-        if (Input.GetMouseButtonDown(0)) { //on left click
-            RaycastHit hit = new RaycastHit();
-
+    void OnMouseDown(){ //make sure other pieces are unselectable during this process
+        if (Input.GetMouseButtonDown(0)){ //on left click
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("is this hitting? " + mousePos);
+            spawnTiles();
         }
+    }
 
+    void spawnTiles(){
+        tile1 = Instantiate(moveTile, new Vector3(this.transform.position.x + 2, this.transform.position.y + 2, 1), Quaternion.identity);
+        tile2 = Instantiate(moveTile, new Vector3(this.transform.position.x - 2, this.transform.position.y + 2, 1), Quaternion.identity);
 
+        tile1.setParent(this);
+        tile2.setParent(this);
+    }
+
+    void destroyTiles(){
+        Destroy(tile1);
+        Destroy(tile2);
+    }
+
+    public void moveMe(Vector3 newPos) {
+        this.transform.position = newPos;
+        destroyTiles();
     }
 }
