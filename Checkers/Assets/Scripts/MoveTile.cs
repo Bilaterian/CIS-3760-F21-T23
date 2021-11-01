f111cd2bffb9d12457b4398faa3c0503492ca0b6
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2021-10-31 22:37:56 -0400

    added single piece removal
    piece capturing has been implemented.

--- Checkers/Assets/Scripts/MoveTile.cs
+++ Checkers/Assets/Scripts/MoveTile.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class MoveTile : MonoBehaviour
 {
     private Piece parent;
+    private bool isKillMove = false;
 
     Vector3 getPos() { 
         return this.transform.position;
     }
 
     void OnMouseDown() {
-        parent.moveMe(getPos());
+        parent.moveMe(getPos(), isKillMove);
         
     }
 
@@ -23,5 +24,9 @@ public class MoveTile : MonoBehaviour
         this.GetComponent<SpriteRenderer>().enabled = false;
         Destroy(this.GetComponent<BoxCollider2D>());
         Destroy(this);
+    }
+
+    public void setKill() {
+        isKillMove = true;
     }
 }
